d5c8a46508477f2bf862dc841838470f2909c455
Md Azharul <mdajharul869@gmail.com>
2025-07-20 15:20:50 +0600

    d

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('✅ Vercel Express API is working!');
 });
 
-PORT = 3000
-app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
-//module.exports = serverless(app);
+//PORT = 3000
+//app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
+module.exports = serverless(app);
